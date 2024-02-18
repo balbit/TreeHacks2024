cb2c14935564ce54ebfb617ed2605a205678a6dc
Alex Hu <superalexanderhu@gmail.com>
2024-02-18 03:21:51 -0800

    add oil_rig

--- drone-3d/public/maings.js
+++ drone-3d/public/maings.js
@@ -731,10 +731,10 @@ async function main() {
     carousel = false;
   } catch (err) {}
   const url = new URL(
-    // "nike.splat",
-    // location.href,
-    params.get("url") || "train.splat",
-    "https://huggingface.co/cakewalk/splat-data/resolve/main/"
+    "oil_rig.splat",
+    location.href,
+    // params.get("url") || "train.splat",
+    // "https://huggingface.co/cakewalk/splat-data/resolve/main/"
   );
   const req = await fetch(url, {
     mode: "cors", // no-cors, *cors, same-origin
